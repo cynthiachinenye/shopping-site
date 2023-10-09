b6ca86e5b48337f79f8c870319a098309f70a17e
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-09 15:23:57 +0100

    new

--- src/navigation/Nav.jsx
+++ src/navigation/Nav.jsx
@@ -1,5 +1,6 @@
 import './nav.css'
-import {AiFillHeart} from 'react-icons/ai'
+import {AiFillHeart,AiOutlineShoppingCart,AiOutlineUserAdd} from 'react-icons/ai'
+
 
 function Nav() {
   return (
@@ -8,7 +9,16 @@ function Nav() {
      <input className='search-input' placeholder='Enter your search shoes'/>
      </div>
      <div className='profile-container'>
-     <a href='#'></a>
+     <a href='#'>
+     <AiFillHeart className='nav-icons'/>
+     </a> 
+     <a href="#">
+     <AiOutlineShoppingCart className='nav-icons'/>
+     </a>
+     <a href='#'>
+     <AiOutlineUserAdd className='nav-icons'/>
+     </a>
+    
      </div>
     </nav>
   )
