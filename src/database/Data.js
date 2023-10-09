30b827a9a72676c44cccd3d94816afd41198cdb3
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-09 11:58:42 +0100

    new

---
+++ src/database/Data.js
@@ -0,0 +1,119 @@
+import {AiFillStar} from 'react-icons/ai'
+
+
+const data = [
+
+    {
+        img:'https://images.unsplash.com/photo-1579338559194-a162d19bf842?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c25lYWtlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
+        name:'  Air Walker One Sneaker',
+        star:< AiFillStar/>,
+        reviews:"(123 reviews)",
+        prevPrice:"₦50,000",
+        newPrice:'₦30,000',
+        color:'white',
+        category:'Sneakers'
+
+    },
+
+    {
+        img:'https://images.unsplash.com/photo-1579338559194-a162d19bf842?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c25lYWtlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
+        name:'  Air Walker One Sneaker',
+        star:< AiFillStar/>,
+        reviews:"(123 reviews)",
+        prevPrice:"₦50,000",
+        newPrice:'₦30,000',
+        color:'white',
+        category:'Sneakers'
+
+    },
+
+    {
+        img:'https://images.unsplash.com/photo-1579338559194-a162d19bf842?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c25lYWtlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
+        name:'  Air Walker One Sneaker',
+        star:< AiFillStar/>,
+        reviews:"(123 reviews)",
+        prevPrice:"₦50,000",
+        newPrice:'₦30,000',
+        color:'white',
+        category:'Sneakers'
+
+    },
+
+    {
+        img:'https://images.unsplash.com/photo-1579338559194-a162d19bf842?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c25lYWtlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
+        name:'  Air Walker One Sneaker',
+        star:< AiFillStar/>,
+        reviews:"(123 reviews)",
+        prevPrice:"₦50,000",
+        newPrice:'₦30,000',
+        color:'white',
+        category:'Sneakers'
+
+    },
+    {
+        img:'https://images.unsplash.com/photo-1579338559194-a162d19bf842?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c25lYWtlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
+        name:'  Air Walker One Sneaker',
+        star:< AiFillStar/>,
+        reviews:"(123 reviews)",
+        prevPrice:"₦50,000",
+        newPrice:'₦30,000',
+        color:'white',
+        category:'Sneakers'
+
+    },
+    {
+        img:'https://images.unsplash.com/photo-1579338559194-a162d19bf842?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c25lYWtlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
+        name:'  Air Walker One Sneaker',
+        star:< AiFillStar/>,
+        reviews:"(123 reviews)",
+        prevPrice:"₦50,000",
+        newPrice:'₦30,000',
+        color:'white',
+        category:'Sneakers'
+
+    },
+    {
+        img:'https://images.unsplash.com/photo-1579338559194-a162d19bf842?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c25lYWtlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
+        name:'  Air Walker One Sneaker',
+        star:< AiFillStar/>,
+        reviews:"(123 reviews)",
+        prevPrice:"₦50,000",
+        newPrice:'₦30,000',
+        color:'white',
+        category:'Sneakers'
+
+    },
+    {
+        img:'https://images.unsplash.com/photo-1579338559194-a162d19bf842?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c25lYWtlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
+        name:'  Air Max Sneaker',
+        star:< AiFillStar/>,
+        reviews:"(123 reviews)",
+        prevPrice:"₦50,000",
+        newPrice:'₦30,000',
+        color:'white',
+        category:'Sneakers'
+
+    },
+    {
+        img:'https://images.unsplash.com/photo-1514989940723-e8e51635b782?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHNuZWFrZXJzfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60',
+        name:'  Air Walker One Sneaker',
+        star:< AiFillStar/>,
+        reviews:"(123 reviews)",
+        prevPrice:"₦50,000",
+        newPrice:'₦30,000',
+        color:'yellow',
+        category:'Sneakers'
+
+    },
+    {
+        img:'https://images.unsplash.com/photo-1549298916-f52d724204b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHNuZWFrZXJzfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60',
+        name:'  Nike One Sneaker',
+        star:< AiFillStar/>,
+        reviews:"(123456 reviews)",
+        prevPrice:"₦150,000",
+        newPrice:'₦100,000',
+        color:'brown',
+        category:'Sneakers'
+
+    },
+]
