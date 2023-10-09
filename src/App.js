efc2249dced62d7c4f3dfa964b384f97402f70ea
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-09 12:55:34 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navigator from './navigation/Nav'
 import Products from './products/Products';
+import Recommend from './recommendations/Recommend';
 
 function App() {
   return (
     <div className="App">
     <Navigator/>
     <Products/>
+    <Recommend/>
       
     </div>
   );
---
+++ src/recommendations/Recommend.jsx
@@ -0,0 +1,9 @@
+import './recommend.css'
+
+function Recommend() {
+  return (
+    <div>Recommend</div>
+  )
+}
+
+export default Recommend
