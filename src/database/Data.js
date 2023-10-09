4bb4cbf90309dc8d6c1e8dd557db9798cc8632ab
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-09 12:09:05 +0100

    new

--- src/database/Data.js
+++ src/database/Data.js
@@ -28,58 +28,58 @@ const data = [
     },
 
     {
-        img:'https://images.unsplash.com/photo-1579338559194-a162d19bf842?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c25lYWtlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
+        img:'https://media.istockphoto.com/id/1144347244/photo/unbranded-black-sport-running-shoes-or-sneakers-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=8VM3Tt8tToj7QTZ0jQEilQpltdet7ZY3wVPyU9llrzQ=',
         name:'  Air Walker One Sneaker',
         star:< AiFillStar/>,
         reviews:"(123 reviews)",
         prevPrice:"₦50,000",
         newPrice:'₦30,000',
-        color:'white',
+        color:' grey Sneaker on isolated white',
         category:'Sneakers'
 
     },
 
     {
-        img:'https://images.unsplash.com/photo-1579338559194-a162d19bf842?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c25lYWtlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
-        name:'  Air Walker One Sneaker',
+        img:'https://media.istockphoto.com/id/1411475205/photo/black-sneaker-stands-on-the-tip-isolated-on-white.webp?b=1&s=170667a&w=0&k=20&c=2gFCOssIR2aIEEyFGV8dU-7ZomkicwDXit54aSDExvk=',
+        name:'Air force Sneakers ',
         star:< AiFillStar/>,
         reviews:"(123 reviews)",
         prevPrice:"₦50,000",
         newPrice:'₦30,000',
-        color:'white',
+        color:'Black Sneaker on isolated white',
         category:'Sneakers'
 
     },
     {
-        img:'https://images.unsplash.com/photo-1579338559194-a162d19bf842?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c25lYWtlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
-        name:'  Air Walker One Sneaker',
+        img:'https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c25lYWtlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
+        name:'  Air Force  1 high',
         star:< AiFillStar/>,
         reviews:"(123 reviews)",
         prevPrice:"₦50,000",
         newPrice:'₦30,000',
-        color:'white',
+        color:'white and blue',
         category:'Sneakers'
 
     },
     {
-        img:'https://images.unsplash.com/photo-1579338559194-a162d19bf842?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c25lYWtlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
-        name:'  Air Walker One Sneaker',
+        img:'https://images.unsplash.com/photo-1607522370275-f14206abe5d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8c25lYWtlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
+        name:'  HD Star',
         star:< AiFillStar/>,
         reviews:"(123 reviews)",
         prevPrice:"₦50,000",
         newPrice:'₦30,000',
-        color:'white',
+        color:'Red',
         category:'Sneakers'
 
     },
     {
-        img:'https://images.unsplash.com/photo-1579338559194-a162d19bf842?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c25lYWtlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
-        name:'  Air Walker One Sneaker',
+        img:'https://images.unsplash.com/photo-1608667508764-33cf0726b13a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHNuZWFrZXJzfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60',
+        name:'  New Sneaker',
         star:< AiFillStar/>,
         reviews:"(123 reviews)",
-        prevPrice:"₦50,000",
-        newPrice:'₦30,000',
-        color:'white',
+        prevPrice:"₦60,000",
+        newPrice:'₦40,000',
+        color:'light-brown',
         category:'Sneakers'
 
     },
