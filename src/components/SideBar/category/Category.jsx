94798623a4627fe574ec1cbdc77208768048fd17
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-12 09:13:41 +0100

    new

--- src/components/SideBar/category/Category.jsx
+++ src/components/SideBar/category/Category.jsx
@@ -2,7 +2,32 @@ import'./category.css'
 
 function Category() {
   return (
-    <div>Category</div>
+    <div>
+    <h2 className='sidebar-title'>Category</h2>
+
+    <div>
+    <label className='sidebar-label-container'>
+    <input type='radio' name='test'/>
+    <span className='checkmark'>All</span>
+    </label>
+    <label className='sidebar-label-container'>
+    <input type='radio' name='test'/>
+    <span className='checkmark'>Sneakers</span>
+    </label>
+    <label className='sidebar-label-container'>
+    <input type='radio' name='test'/>
+    <span className='checkmark'>Flats</span>
+    </label>
+    <label className='sidebar-label-container'>
+    <input type='radio' name='test'/>
+    <span className='checkmark'>Sandals</span>
+    </label>
+    <label className='sidebar-label-container'>
+    <input type='radio' name='test'/>
+    <span className='checkmark'>Heels</span>
+    </label>
+    </div>
+    </div>
   )
 }
 
