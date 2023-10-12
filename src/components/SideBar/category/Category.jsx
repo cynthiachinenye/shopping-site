220d1db6023045b3655d099921f338fbcd0db01f
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-12 11:37:27 +0100

    new

--- src/components/SideBar/category/Category.jsx
+++ src/components/SideBar/category/Category.jsx
@@ -6,25 +6,27 @@ function Category() {
     <h2 className='sidebar-title'>Category</h2>
 
     <div>
+   
+    <label className='sidebar-label-container' for="test">
+    <input type='radio' name='test' id="test" /> 
+     <span className='checkmark'></span>All
+     </label>
+    
     <label className='sidebar-label-container'>
     <input type='radio' name='test'/>
-    <span className='checkmark'>All</span>
+    <span className='checkmark'></span>Sneakers
     </label>
     <label className='sidebar-label-container'>
     <input type='radio' name='test'/>
-    <span className='checkmark'>Sneakers</span>
+    <span className='checkmark'></span>Flats
     </label>
     <label className='sidebar-label-container'>
     <input type='radio' name='test'/>
-    <span className='checkmark'>Flats</span>
+    <span className='checkmark'></span>Sandals
     </label>
     <label className='sidebar-label-container'>
     <input type='radio' name='test'/>
-    <span className='checkmark'>Sandals</span>
-    </label>
-    <label className='sidebar-label-container'>
-    <input type='radio' name='test'/>
-    <span className='checkmark'>Heels</span>
+    <span className='checkmark'></span>Heels
     </label>
     </div>
     </div>
