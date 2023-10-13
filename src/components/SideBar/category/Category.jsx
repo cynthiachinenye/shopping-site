c38bcbf847841e7d2e97225bb4ccd2ee9be39784
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-13 14:34:53 +0100

    new

--- src/components/SideBar/category/Category.jsx
+++ src/components/SideBar/category/Category.jsx
@@ -1,16 +1,35 @@
 import Inputs from '../../Inputs'
 import'./category.css'
 
-function Category() {
+function Category({handleChange}) {
   return (
     <div>
     <h2 className='sidebar-title'>Category</h2>
 
     <div>
-     <Inputs/>
-     <Inputs/>
-     <Inputs/>
-     <Inputs/>
+    <label className='sidebar-label-container'>
+    <input onChange={handleChange} type= 'radio' value="" name=""/>
+    <span className='checkmark'></span>All
+    </label>
+    <Inputs
+     handleChange={handleChange}
+     value="sneakers"
+     title="Sneakers"
+     name="test"
+    />
+    <Inputs
+     handleChange={handleChange}
+     value="falts"
+     title="Sneakers"
+     name="test"
+    />
+    <Inputs
+    handleChange={handleChange}
+    value="sneakers"
+    title="Sneakers"
+    name="test"
+   />
+
     </div>
     </div>
   )
